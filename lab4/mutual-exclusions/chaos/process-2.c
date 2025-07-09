4c04aba53d6196dbd88705ff0633db9980138723
thientc <thientc84@gmail.com>
2025-07-09 05:57:10 +0000

    add code

---
+++ lab4/mutual-exclusions/chaos/process-2.c
@@ -0,0 +1,17 @@
+/*process-2.c*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+int main(int argc, char * argv[]) {
+    char * c = "Process 2 - This is operating system course.\n";
+    setbuf(stderr, NULL);  
+    while (* c != '\0') {
+        fputc(* c, stderr);
+        c++;
+        sleep(rand()%2+1);
+    }
+    return 0;
+}
